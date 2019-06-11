7b7e493808bedcceb5e19d24e3c5c87412389dd2
raj297 <sanganabatlaraj297@gmail.com>
2019-06-11 13:14:21 +0530

    added add

---
+++ MyPOS.Android/Implementations/AndroidSQLite.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using SQLite.Net;
+using MyPOS.Droid.Implementaions;
+using MyPOS.Helpers;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AndroidSQLite))]
+namespace MyPOS.Droid.Implementaions
+{
+    public class AndroidSQLite : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            
+            // Documents folder
+            var path = Path.Combine(documentsPath, DatabaseHelper.DbFileName);
+            var plat = new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid();
+            var conn = new SQLiteConnection(plat, path);
+
+            // Return the database connection
+            return conn;
+        }
+    }
+}
